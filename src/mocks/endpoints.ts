bc514c322313abadbb2a253dd93e6be5921bf51b
Toni Skoro <69433943+yousoezs@users.noreply.github.com>
2024-08-09 18:07:58 +0200

    * Implements method getHouseGenerator POC Logic

--- src/mocks/endpoints.ts
+++ src/mocks/endpoints.ts
@@ -1,9 +1,38 @@
 import { House } from "managers/HouseManager/HouseManager.types";
 
 export const getHousesGenerator: () => House[] = () => {
+
+  const maxHouses = 10;
+  const amountOfHouses = Math.floor(Math.random() * maxHouses);
+  const randomHouseVal = amountOfHouses ? amountOfHouses : 1;
+
+  const houses = [];
+  for (let i = 0; i < randomHouseVal; i++) {
+
+    const maxPoints = 4;
+    const pointsRandomVal = []
+    const houseRandomValRot = [0, Math.floor(Math.random() * 360), 0];
+    const maxRandHeight = 20;
+    const randomHeight = Math.floor(Math.random() * maxRandHeight);
+
+    for(let j = 0; j < maxPoints; j++) {
+      const randomXPos = Math.floor(Math.random() * 25);
+      const randomZPos = Math.floor(Math.random() * 25);
+
+      pointsRandomVal.push([randomXPos, 0, randomZPos]);
+    }
+
+    const house = {
+      points: pointsRandomVal,
+      position: pointsRandomVal[0],
+      rotation: houseRandomValRot,
+      height: randomHeight,
+    } as House;
+    houses.push(house);
+  }
   /** IMPLEMENT:
    * Implement the generator function according to the objectives of the use case.
    */
 
-  return [];
+  return houses;
 };
